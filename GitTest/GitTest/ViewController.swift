49beece6f1d841b0c11b6767959399f2607bd654
Michael Fuhrmann <michaelfuhrmann@Freigabe.local>
2014-09-20 23:27:41 +0200

    New Textes and Commands

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Michael Fuhrmann on 20.09.14.
+//  Copyright (c) 2014 Michael Fuhrmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var testLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
